260acf41a1e0ecbb8f602a21088e7ff3e0bca5db
kimkiyong <fenderst0612@gmail.com>
2023-05-27 19:07:38 +0900

    feat: display jwt

--- app.js
+++ app.js
@@ -1,65 +1,68 @@
-import {initializeApp} from "firebase/app"
+import { initializeApp } from "firebase/app";
 import {
   getAuth,
   onAuthStateChanged,
   GoogleAuthProvider,
   signInWithRedirect,
   signOut,
-} from "firebase/auth"
-import {firebaseConfig} from './firebase-config.js'
+} from "firebase/auth";
+import { firebaseConfig } from "./firebase-config.js";
 
-main()
+main();
 
-async function main () {
+async function main() {
   try {
     const el = {
-      sectionSignin: document.querySelector('#sectionSignin'),
-      sectionUser: document.querySelector('#sectionUser'),
-      sectionSignout: document.querySelector('#sectionSignout'),
+      sectionSignin: document.querySelector("#sectionSignin"),
+      sectionUser: document.querySelector("#sectionUser"),
+      sectionSignout: document.querySelector("#sectionSignout"),
 
-      buttonSignin: document.querySelector('#buttonSignin'),
-      buttonSignout: document.querySelector('#buttonSignout'),
+      buttonSignin: document.querySelector("#buttonSignin"),
+      buttonSignout: document.querySelector("#buttonSignout"),
 
-      uid: document.querySelector('#uid'),
-    }
-    const app = initializeApp(firebaseConfig) // <2>
-    const auth = getAuth(app) // <3>
+      jwt: document.querySelector("#jwt"),
+    };
+    const app = initializeApp(firebaseConfig); // <2>
+    const auth = getAuth(app); // <3>
 
-    onAuthStateChanged(auth, (user) => { // <4>
+    onAuthStateChanged(auth, (user) => {
+      // <4>
       if (user) {
-        el.sectionSignin.style.display = 'none'
-        el.sectionUser.style.display = 'block'
-        el.sectionSignout.style.display = 'block'
-        el.uid.innerHTML = user.uid
-        console.log(user)
+        el.sectionSignin.style.display = "none";
+        el.sectionUser.style.display = "block";
+        el.sectionSignout.style.display = "block";
+        el.jwt.innerHTML = "Bearer " + user.accessToken;
+        console.log(user);
       } else {
-        el.sectionSignin.style.display = 'block'
-        el.sectionUser.style.display = 'none'
-        el.sectionSignout.style.display = 'none'
+        el.sectionSignin.style.display = "block";
+        el.sectionUser.style.display = "none";
+        el.sectionSignout.style.display = "none";
       }
-    })
+    });
 
-    el.buttonSignin.addEventListener('click', async (event) => { // <5>
+    el.buttonSignin.addEventListener("click", async (event) => {
+      // <5>
       try {
-        event.preventDefault()
+        event.preventDefault();
 
-        const provider = new GoogleAuthProvider()
-        await signInWithRedirect(auth, provider)
+        const provider = new GoogleAuthProvider();
+        await signInWithRedirect(auth, provider);
       } catch (err) {
-        el.errorMessage.innerHTML = err.message
-        console.error(err)
+        el.errorMessage.innerHTML = err.message;
+        console.error(err);
       }
-    })
+    });
 
-    el.buttonSignout.addEventListener('click', async (event) => { // <6>
+    el.buttonSignout.addEventListener("click", async (event) => {
+      // <6>
       try {
-        event.preventDefault()
-        await signOut(auth)
+        event.preventDefault();
+        await signOut(auth);
       } catch (err) {
-        console.error(err)
+        console.error(err);
       }
-    })
+    });
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
 }
